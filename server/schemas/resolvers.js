d8b988b4fd22288e6658822e991c261c1a50d044
mambru <ecastro82@gmail.com>
2021-05-15 07:11:03 -0500

    typeDefs established, resolvers file pseudocoded

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,15 @@
+const { User, Book } = require('../models')
+
+const resolvers = {
+    Query: {
+//get a single user by either id or username
+    },
+
+    Mutation: {
+
+//login a single user
+// create a user
+// save a book
+// delete a book 
+    }
+}
