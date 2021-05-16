75ec2b514540d61b1a0903f19c7e05e143145003
mambru <ecastro82@gmail.com>
2021-05-16 12:09:14 -0500

    added react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { ApolloProvider } from '@apollo/react-hooks';
 import ApolloClient from 'apollo-boost';
 
@@ -13,14 +14,14 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-      <>
+      <Router>
         <Navbar />
         <Switch>
           <Route exact path='/' component={SearchBooks} />
           <Route exact path='/saved' component={SavedBooks} />
           <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
         </Switch>
-      </>
+      </Router>
     </ApolloProvider>
   );
 }
